3732a6a96b43e7eef6393958f1ee45692cf204e0
g0294 <g0294@edaworld.com.tw>
2016-01-26 13:50:13 +0800

    Activity - > AppCompatActivity

--- app/src/main/java/com/example/g0294/tutorial/gmap/gMapMenu.java
+++ app/src/main/java/com/example/g0294/tutorial/gmap/gMapMenu.java
@@ -1,14 +1,14 @@
 package com.example.g0294.tutorial.gmap;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class gMapMenu extends Activity {
+public class gMapMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
